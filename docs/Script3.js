dc6787e885ac2d7eec434570b14721d61454dd8c
Dev-Torisima <toridevcon@outlook.jp>
2025-04-20 11:23:17 +0900

    Add files via upload

---
+++ docs/Script3.js
@@ -0,0 +1,21 @@
+//Script3.js
+//Created by Torisima 2025
+//Script1.js => Script3.js => Script5.js
+
+var e11 = document.getElementsByClassName("e11")[0];
+var e12 = document.getElementsByClassName("e12")[0];
+
+const v5 = 2;
+var v6 = 0;
+var v7 = 0;
+
+
+e11.addEventListener('change', ()=>
+    {
+        v6 = e11.value;
+    });
+
+    e12.addEventListener('change', ()=>
+        {
+            v7 = e12.value;
+        });
---
+++ docs/Script4.js
@@ -0,0 +1,7 @@
+//Script4.js
+//Created by Torisima 2025
+//Script1.js => Script4.js => Script5.js
+
+const v5 = 1;
+const v6 = 0x01;
+const v7 = 0x20;
